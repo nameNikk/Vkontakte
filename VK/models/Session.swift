b1e3ec79202bc76ecc8008fddd147e92bab533c1
NikMak <mak.nik.2014@yandex.ru>
2021-03-16 17:45:15 +0300

    singleton(ex1)
    добавление класса сессия с синглтоном

---
+++ VK/models/Session.swift
@@ -0,0 +1,25 @@
+//
+//  Session.swift
+//  VK
+//
+//  Created by Никита Макаренко on 16.03.2021.
+//
+
+import UIKit
+
+
+class Session {
+    
+    static let shared = Session()
+    
+    
+    private init() {}
+        
+    var token = ""
+    var userld = 0
+    
+    
+}
+
+
+
